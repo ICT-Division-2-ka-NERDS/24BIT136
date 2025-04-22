7157fbac8fa6f0a140ac42489a28d7db4a24412b
RudraMonpara <rudramonpara2345@gmail.com>
2025-04-23 00:38:43 +0530

    Delete lab10/new.py

--- lab10/new.py
+++
@@ -1,9 +0,0 @@
-def try2():
-    try:
-        a = int(input("Enter an integer"))
-        b = int(input("Enter another integer"))
-        c = a / b
-        print ("Division = ", c)
-    except ZeroDivisionError:
-        print ("Division by zero error")    
-try2()
